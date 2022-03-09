9dfc10dd584482abe7f47be4aaa9d3ae6a86d570
Vishnu <82942312+vishnuqz12@users.noreply.github.com>
2022-03-09 20:05:42 +0530

    Update petclinic.js

--- frontend/src/components/customer/petclinic/petclinic.js
+++ frontend/src/components/customer/petclinic/petclinic.js
@@ -10,7 +10,7 @@ function Petclinic(){
 
   
     useEffect(()=>{
-      Axios.get("http://172.31.4.239:5000/api/petclinics")
+      Axios.get("http://65.0.180.125:5000/api/petclinics")
       .then(res=>{
           console.log(res.data);
           setClinic(res.data)
